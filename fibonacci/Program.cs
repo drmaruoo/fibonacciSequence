ee4feb71f61782738ec5a7a79b79636f0cfa89bf
Marcin Domarski <31709302+drmaruoo@users.noreply.github.com>
2017-09-06 23:30:40 +0200

    creation

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -10,6 +10,31 @@ namespace fibonacci
     {
         static void Main(string[] args)
         {
+            bool enabled = true;
+            int n = 0;
+            Console.WriteLine("Press [ENTER] to generate next number, write anything and press [ENTER] to close");
+            while (enabled)
+            {
+                if (Console.ReadLine() != "")
+                {
+                    enabled = false;
+                }
+                else
+                {
+                    Console.Write(generate(n++));
+                }
+            }
+        }
+        static int generate(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else
+            {
+                return generate(n - 2) + generate(n - 1);
+            }
         }
     }
 }
