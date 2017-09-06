04600c76c0e46896f3a50fb034f8f039e2a28d5c
Marcin Domarski <31709302+drmaruoo@users.noreply.github.com>
2017-09-07 00:39:04 +0200

    Added new algorithm, stopwatch, memory usage counter, changed ulong to BigInteger, made code more spaghetti-like.

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Numerics;
 
 namespace fibonacci
 {
@@ -10,31 +9,72 @@ namespace fibonacci
     {
         static void Main(string[] args)
         {
+            Stopwatch stopWatch = new Stopwatch();
             bool enabled = true;
+            bool recursion = true;
+            List<BigInteger> numbers = new List<BigInteger>();
             int n = 0;
-            Console.WriteLine("Press [ENTER] to generate next number, write anything and press [ENTER] to close");
+            Console.WriteLine("Press [r] to use recursive algorithm or press [a] to use alternative one");
+            if (Console.ReadKey().KeyChar == 'a')
+            {
+                Console.Clear();
+                recursion = false;
+                Console.WriteLine("Using alternative algorithm");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Using recursive algorithm");
+            }
+
+            Console.WriteLine("Press [ENTER] to generate next number, write anything and press [ENTER] to exit");
+            Console.WriteLine("Hold [ENTER] to generate numbers quickly");
+            long initial = GC.GetTotalMemory(true);
             while (enabled)
             {
+                stopWatch.Reset();
                 if (Console.ReadLine() != "")
                 {
                     enabled = false;
                 }
                 else
                 {
-                    Console.Write(generate(n++));
+                    stopWatch.Start();
+                    if (recursion) Console.Write(generate(n++));
+                    else
+                    {
+                        BigInteger temp = generateAlternative(n++, numbers);
+                        numbers.Add(temp);
+                        Console.Write(temp);
+                    }
+                    stopWatch.Stop();
+                    long size = (GC.GetTotalMemory(true) - initial);
+                    Console.Write(" | it took " + stopWatch.ElapsedMilliseconds + "ms | " + size + "B of memory used");
                 }
             }
         }
-        static int generate(int n)
+        static BigInteger generate(int n)
         {
             if (n < 2)
             {
-                return n;
+                return (BigInteger)n;
             }
             else
             {
                 return generate(n - 2) + generate(n - 1);
             }
         }
+        static BigInteger generateAlternative(int n, List<BigInteger> array)
+        {
+            if (n < 2)
+            {
+                return (BigInteger)n;
+            }
+            else
+            {
+                return array[n - 2] + array[n - 1];
+            }
+        }
+
     }
 }
